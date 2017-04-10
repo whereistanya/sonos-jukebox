f91c1efe0327a3465231d51049cec2d5ec81b21d
Tanya Reilly <tanya.reilly@gmail.com>
2017-04-10 19:48:17 -0400

    More buttons.

--- buttons.py
+++ buttons.py
@@ -7,10 +7,14 @@
 MACS = {
     'ac:63:be:8f:62:09': 'glad',
     '44:65:0d:ab:a6:07': 'playdoh',
+    '40:b4:cd:9c:b7:cd': 'ziploc',
+    '40:b4:cd:d0:a0:24': 'brooklyn',
 }
 
 # play_local, directory_to_play, device
 COMMANDS = {
     'glad': ['play_local', 'hamilton', 'Living Room'],
     'playdoh': ['play_local', 'moana', 'Living Room'],
+    'ziploc': ['play_local', 'lemonade', 'Living Room'],
+    'brooklyn': ['play_local', 'bach', 'Living Room'],
 }
