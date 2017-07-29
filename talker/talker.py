ba65c23727c1697566375fe771d610d8c190eab7
Tanya Reilly <tanya.reilly@gmail.com>
2017-07-29 19:22:10 -0400

    Text to speech for sonos.

---
+++ talker/talker.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python2.7
+
+"""
+Have your Sonos say words. Converts the text to mp3, spins up a webserver to
+serve the mp3, sends the mp3 url to the Sonos, shuts down again.
+
+The local webserver (very briefly) serves everything in the directory the script
+is running in, so don't run this from your super secret data directory or
+whatever.
+
+You'll need to change the SONOS_NAME global variable and maybe also the port if
+you're using 8081 for something.
+"""
+
+import os
+import sys
+import time
+import urllib
+
+# Need to pip install gtts and soco.
+from gtts import gTTS
+from soco import SoCo
+
+sys.path.append("..")
+import localwebserver
+import my_ip
+import sonos
+
+SONOS_NAME = "Living Room"
+PORT = 8081
+
+def main():
+  ipaddr = my_ip.lookup()
+  player = sonos.Player()
+
+  webserver = localwebserver.HttpServer(PORT)
+  webserver.start()
+
+  uri = "http://%s:%s/" % (ipaddr, PORT)
+
+  if len(sys.argv) > 1:
+    text = str(sys.argv[1:])
+  else:
+    text = "I am a talking computer"
+
+  filename = "words.mp3"
+  tts = gTTS(text=text, lang='en')
+  tts.save(filename)
+
+  url = os.path.join(uri, urllib.pathname2url(filename))
+  player.play([url,], SONOS_NAME)
+  # Give the sonos a moment to grab the mp3 before killing the webserver.
+  time.sleep(1)
+
+main()
